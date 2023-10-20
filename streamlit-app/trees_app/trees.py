c67e28c5633c6b6b2dd09f809eac0ebae0904a7f
Jeff-Barlow-Spady <135166969+Jeff-Barlow-Spady@users.noreply.github.com>
2023-10-20 22:06:00 +0000

    commit

--- streamlit-app/trees_app/trees.py
+++ streamlit-app/trees_app/trees.py
@@ -5,6 +5,8 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import datetime as dt
+from bokeh.plotting import figure
+import altair as alt
 
 st.title("The Trees of San Fransisco")
 
@@ -33,9 +35,9 @@ st.divider()
 st.subheader("Mapping")
 
 st.divider()
-trees_df = trees_df.dropna(subset=["longitude", "latitude"])
-trees_df = trees_df.sample(n=1000)
-st.map(trees_df)
+trees_df_sub = trees_df.dropna(subset=["longitude", "latitude"])
+trees_df_sub = trees_df_sub.sample(n=1000)
+st.map(trees_df_sub)
 st.divider()
 ########################################################################################
 st.subheader("Plotly")
@@ -45,5 +47,31 @@ fig = px.histogram(trees_df["dbh"])
 st.plotly_chart(fig)
 
 st.divider()
-st.subheader("Seaborn")
+################################################################################################
 trees_df['age'] = (pd.to_datetime('today') - pd.to_datetime(trees_df['date'])).dt.days
+st.subheader("Seaborn")
+fig_sb, ax_sb = plt.subplots()
+ax_sb = sns.histplot(trees_df['age'])
+plt.xlabel('Age(Days)')
+st.pyplot(fig_sb)
+
+st.subheader('Matplotlib')
+fig_mpl, ax_mpl = plt.subplots()
+ax_mpl = plt.hist(trees_df['age'])
+plt.xlabel("Age (Days)")
+st.pyplot(fig_mpl)
+st.divider()
+
+st.subheader('Bokeh')
+scatterplot = figure(title = 'Bokeh Scatterplot')
+scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
+scatterplot.yaxis.axis_label = "site_order"
+scatterplot.xaxis.axis_label = "dbh"
+st.bokeh_chart(scatterplot)
+st.divider()
+#########################################################################
+st.subheader("Altair")
+#df_ctkr = trees_df.groupby(["caretaker"]).count()["tree_id"].reset_index()
+#df_ctkr.column = ['caretaker', 'tree_count']
+fig = alt.Chart(trees_df).mark_bar().encode(x= 'caretaker', y = 'count(*):Q')
+st.altair_chart(fig)
