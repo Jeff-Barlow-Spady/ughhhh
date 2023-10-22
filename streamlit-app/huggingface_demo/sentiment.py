1d342219c3fe223d6160cab5b919989cc77e6d46
Jeff-Barlow-Spady <135166969+Jeff-Barlow-Spady@users.noreply.github.com>
2023-10-22 03:11:07 +0000

    wefw

---
+++ streamlit-app/huggingface_demo/sentiment.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from transformers import pipeline
+
+st.title("Hugging Face Demo: Sentiment Analysis")
+
+text = st.text_input("Enter Text to Analyze")
+@st.cache_resource()
+def get_model()
+    return pipeline("sentiment-analysis")
+model = get_model()
+    if text:
+        result = model(text)
+        st.write("Sentiment", result[0]["label"])
+        st.write("Confidence", result[0]["score"])
