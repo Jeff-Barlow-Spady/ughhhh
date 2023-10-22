854757db8fe6646fee5ee780bdfe20c263df7a1c
Jeff-Barlow-Spady <135166969+Jeff-Barlow-Spady@users.noreply.github.com>
2023-10-22 03:35:37 +0000

    egr

--- streamlit-app/penguin_ml/penguins_streamlit.py
+++ streamlit-app/penguin_ml/penguins_streamlit.py
@@ -23,8 +23,8 @@ st.write(
 penguin_file = st.file_uploader("Upload your own penguin data")
 
 if penguin_file is None:
-    rf_pickle = open("random_forest_penguin.pickle", "rb")
-    map_pickle = open("output_penguin.pickle", "rb")
+    rf_pickle = open(r"/ughhhh/streamlit-app/penguin_ml/random_forest_penguin.pickle", "rb")
+    map_pickle = open(r"/ughhhh/streamlit-app/penguin_ml/output_penguin.pickle", "rb")
     rfc = pickle.load(rf_pickle)
     unique_penguin_mapping = pickle.load(map_pickle)
     rf_pickle.close()
